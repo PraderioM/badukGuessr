1d5d6a66eccf9f704636e9aca858e7c392cdc18f
PraderioM <PraderioM@Gmail.com>
2022-10-28 00:55:34 +0100

    Made it so that if there are n other guesses with probability half ot that of the current ones then guesses should be added. - close #2.

--- src/app/central-section/utils.ts
+++ src/app/central-section/utils.ts
@@ -1,10 +1,11 @@
+import * as matrixInverse from 'matrix-inverse';
+
 export const guessNames = ['A', 'B', 'C', 'D'];
 export const maxGuesses = guessNames.length;
 export const startingMoves = 4;
 export const showScoreFrequency = 50;
 export const boardSize = 19;
-
-const pointsPerGuess = 30;
+const pointsPerSingleGuess = 30;
 
 export function getEarnedPoints(correctGuess: number, nGuesses: number) {
   // Points are earned only if the correct guess is one of among the guesses made.
@@ -16,11 +17,87 @@ export function getEarnedPoints(correctGuess: number, nGuesses: number) {
 }
 
 export function getGuessPoints(guess: number, nGuesses: number) {
-  // For each move the total number of points is fixed and it is distributed among all the guesses so that all guesses have a number
-  // of points corresponding to a multiple of the points 'x' attributed to the last guess. Each guess gets strictly more points that the
-  // next and the first guess gets nGuesses*x points. Points are rounded to the nearest strictly positive integer.
-  const minPoints = 2 * pointsPerGuess / (nGuesses * (nGuesses + 1));
-  const earnedPoints = (nGuesses - guess) * minPoints;
+  return getAllGuessPoints(nGuesses)[guess];
+}
 
-  return Math.max(1, Math.round(earnedPoints));
+export function getAllGuessPoints(nGuesses: number) {
+  // Get all points without rounding.
+  const guessPoints = getGuessPointList(nGuesses);
+
+  // Round points and give them a minimum of 1 in length.
+  for (let i = 0; i < guessPoints.length; i++) {
+    guessPoints[i] = Math.max(1, Math.round(guessPoints[i]));
+  }
+
+  return guessPoints;
+}
+
+function getGuessPointList(n: number) {
+  const pointSums = [pointsPerSingleGuess];
+  let allPoints: number[] = [pointsPerSingleGuess];
+
+  while (pointSums.length < n) {
+    const LEMatrix = getLEMatrix(pointSums.length + 1);
+    const inverse = matrixInverse(LEMatrix);
+    pointSums.push(getLastSum(pointSums));
+    allPoints = multiplyMatrices(inverse, pointSums);
+    pointSums.pop();
+    pointSums.push(addList(allPoints));
+  }
+
+  return allPoints;
+}
+
+
+function getLEMatrix(n: number, threshold: number = 1 / 2) {
+  const matrix = [];
+  for (let i = 0; i < n - 1; i++) {
+    const row = [];
+    for (let j = 0; j < n; j++) {
+      if (j <= i) {
+        row.push(1);
+      } else {
+        row.push(threshold);
+      }
+    }
+    matrix.push(row);
+  }
+
+  // Last row is used in order to keep the choices evened out so I try to match alternate sum with n / 2 times the last value.
+  matrix.push(getLastRow(n));
+
+  return matrix;
+}
+
+function getLastRow(n: number) {
+  const lastRow = [];
+  for (let i = 0; i < n - 1; i++) {
+    lastRow.push(0);
+  }
+  lastRow.push(1);
+  return lastRow;
+}
+
+function getLastSum(prevSums: number[]) {
+  return prevSums[0] * 0.5 ** (prevSums.length);
+}
+
+function multiplyMatrices(matrix: number[][], column: number[]) {
+  const result = [];
+  for (const row of matrix) {
+    let sum = 0;
+    for (let j = 0; j < column.length; j++) {
+      sum += row[j] * column[j];
+    }
+    result.push(sum);
+  }
+  return result;
+}
+
+function addList(l: number[]) {
+  let sum = 0;
+  for (const x of l) {
+    sum += x;
+  }
+  return sum;
 }
